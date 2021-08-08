4f52150eec5dfae267c84ce674a2ce8a754c08f3
송진하 <songjinha@songjincBookPro.kornet>
2021-08-08 13:07:05 +0900

    css arrow up

--- src/main.js
+++ src/main.js
@@ -33,7 +33,30 @@ contact.addEventListener('click', (event) => {
     scrollIntoView('#contact');
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+   home.style.opacity= 1-window.scrollY / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" button
+const arrow = document.querySelector('.arrow-up');
+document.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
 }
+
